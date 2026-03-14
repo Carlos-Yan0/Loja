1559125c7f5d1436a6c41db8986043146ce3b6ea
Yan Carlos <yan.c.oliveira08@gmail.com>
2026-03-14 19:34:10 -0300

    feat: add logout route

--- apps/backend/src/routes/auth.routes.js
+++ apps/backend/src/routes/auth.routes.js
@@ -5,5 +5,6 @@ const router = Router();
 
 router.post('/refresh', authController.refreshToken);
 router.post('/login', authController.login);
+router.post('/logout', authController.logout);
 
 export {router};
